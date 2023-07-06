40b293cb1b230056987c55042d2a313493626f39
Oleksandr Radoshniuk <radosh.dev@gmail.com>
2023-07-06 14:39:24 +0300

    add ability to login and register via soc media

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -32,9 +32,8 @@ const credentialsProvider = CredentialsProvider({
 		password: { label: "Password", type: "password" },
 	},
 	authorize: async credentials => {
-		console.log("Cred Authorize", credentials)
-
 		if (!credentials?.email || !credentials.password) return null
+
 		const { email, password } = credentials
 		const user = await prisma.user.findUnique({
 			where: { email },
@@ -46,8 +45,9 @@ const credentialsProvider = CredentialsProvider({
 			where: { userId: user.id, provider: "credentials" },
 		})
 
-		if (!account?.password)
+		if (!account?.password) {
 			throw new Error("User is not registered using credentials")
+		}
 
 		const isPwdCompare = await compare(password, account.password)
 
@@ -72,38 +72,77 @@ export const authOptions: NextAuthOptions = {
 	},
 	callbacks: {
 		signIn: async params => {
-			console.log("callback SignIn", params)
-			// const {account, user} = params
-			// if (user.email && account) {
-			// 	// const dbUser = await prisma.user.findUnique({where: {email: user.email}})
-			// 	return true
-			// }
+			try {
+				const { account, user } = params
+				if (!user.email || !account) return false
 
-			return true
+				if (account.provider === "credentials") return true
+
+				const existUser = await prisma.user.findUnique({
+					where: { email: user.email },
+				})
+
+				if (!existUser) {
+					const newUser = await prisma.user.create({
+						data: { email: user.email, name: user.name, image: user.image },
+					})
+					await prisma.account.create({
+						data: {
+							provider: account.provider,
+							providerAccountId: account.providerAccountId,
+							userId: newUser.id,
+						},
+					})
+					return true
+				}
+
+				const existAccount = await prisma.account.findUnique({
+					where: {
+						userId_provider: {
+							provider: account.provider,
+							userId: existUser.id,
+						},
+					},
+				})
+
+				if (!existAccount) {
+					await prisma.account.create({
+						data: {
+							provider: account.provider,
+							providerAccountId: account.providerAccountId,
+							userId: existUser.id,
+						},
+					})
+					return true
+				}
+
+				return true
+			} catch (error) {
+				console.error("SignIn Callback error:", error)
+				return false
+			}
 		},
 		jwt: async ({ token, user, account }) => {
-			console.log("JWT Callback", token, user, account)
 			if (!user?.email) return token
+
 			if (account?.provider === "credentials") {
 				return { ...token, role: user.role, id: user.id }
 			}
+
 			const existUser = await prisma.user.findUnique({
 				where: { email: user.email },
 			})
+
 			return { ...token, role: existUser?.role, id: existUser?.id }
 		},
-		session: ({ session, user, token }) => {
-			console.log("Session Callback", session, user, token)
-
-			return {
-				...session,
-				user: {
-					...session.user,
-					id: token.id,
-					role: token.role,
-				},
-			}
-		},
+		session: ({ session, token }) => ({
+			...session,
+			user: {
+				...session.user,
+				id: token.id,
+				role: token.role,
+			},
+		}),
 	},
 	providers,
 }
