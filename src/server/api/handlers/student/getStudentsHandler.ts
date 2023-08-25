3f8b1e63e34719d5c9d23ba84ac21a51a84956e6
radoshDev <radosh.dev@gmail.com>
2023-08-26 00:14:44 +0300

    Fix students sorting

--- src/server/api/handlers/student/getStudentsHandler.ts
+++ src/server/api/handlers/student/getStudentsHandler.ts
@@ -4,5 +4,6 @@ import { protectedProcedure } from "../../trpc"
 export const getStudentsHandler = protectedProcedure.query(({ ctx }) => {
 	return prisma.student.findMany({
 		where: { userId: ctx.session!.user.id },
+		orderBy: { name: "asc" },
 	})
 })
